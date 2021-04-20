095a9b394b0d312c031749535f4d7e50738fe986
Eric Malley <EMalley998@gmail.com>
2021-04-19 20:11:57 -0400

    appending bug fixed

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function buildTable(response) {
         array.push(item);
     });
 
- $("#results").DataTable({
+    $("#results").DataTable({
         data: array,
         columns: [
             {
@@ -53,10 +53,10 @@ function buildTable(response) {
             },
             { "data": "test" },
             { "data": "url" },
-            
+
         ],
         columnDefs: [
-            
+
             // {
             //     targets: 1,
             //     "width": "20",
@@ -64,24 +64,23 @@ function buildTable(response) {
             //         // console.log(data)
 
             //         r.name = "<a href=" + r.url + ">" + r.name + "</a>"
-                   
+
             //         return r.name;
             //     },
-                // render: function (d, t, r) { return d.substr(0, 30); },
-                // targets: 1,
-               
+            // render: function (d, t, r) { return d.substr(0, 30); },
+            // targets: 1,
 
-           //},
+
+            //},
             { targets: 1, render: function (d, t, r) { return d.substr(0, 30); } }
 
         ],
         "deferRender": true,
         initComplete: function () {
-
             var abv = this.api().column(3);
             var columns = this.api($("#results")).data();
 
-            var name = $('<select class="filter"><option class="subject" value="">All</option></select>')
+            var name = $('<select id="test" class="filter"><option class="subject" value="">All</option></select>')
             name.appendTo('#triggerName')
                 .on('change', function () {
                     var val = $(this).val();
@@ -92,18 +91,16 @@ function buildTable(response) {
             var selects = $('<select class="filter"><option value="">All</option></select>')
                 .appendTo('#triggerAbv')
                 .on('change', function () {
+                    name.empty().append("<option value=>All</option>")
                     var val = $(this).val();
                     // val = val.search("^"+this.value + "$", true,true,false)
                     abv.search(val ? '^' + $(this).val() + '$' : val, true, false).draw()
                     for (var i = 0; i < columns.length; i++) {
                         var row = columns[i];
-                        console.log(row);
-                        console.log(val)
-                        if (val === row.abv) {
-                            //name.empty();
-                            name.append('<option >' + row.description + '</option>')
-                            
+                        if (val == row.abv) {
+                            name.append('<option>' + row.description + '</option>');
                         }
+
                     }
                 });
 
@@ -128,7 +125,7 @@ function buildTable(response) {
                             //console.log(row)
                             //console.log(abvs)
                             if (d === a) {
-                                selects.append('<option value="' + a + '">' + b + '</option>')
+                                selects.append('<option value="' + d + '">' + b + '</option>')
                             }
                         }
 
@@ -149,68 +146,3 @@ function buildTable(response) {
 $(document).ready(function () {
     getData();
 })
-
-
-
-
-
-// columns.data().rows().toArray().forEach(function (t) {
-    //     t = t.split('""');
-    //     t.forEach(function (h) {
-        //         if (!~names.indexOf(h)) {
-            //             names.push(h)
-
-            //         }
-            //     })
-            // });
-            //console.log(i)
-
-
-
-            //     /////////////////////////
-            //     var foodObj = {
-                //         ft: "French Toast",
-                //         bb: "Berry's Berry",
-                //         bw: "Belgian Waffles",
-                //         hb: "Homestyle Breakfast",
-                //         sbw: "StrawBerry Belgian Waffles",
-                //         habcabc: "HomeStle Test"
-                //     }
-                //     $.each(foodObj, function (a, b) {
-                    //         var names = [];
-                    //         var abvs = []
-                    //         var newObj = {}
-
-                    //         //get description based off of the ABV chosen. 
-                    //         columns.data().toArray().forEach(function (t) {
-                        //             t = t.split(',');
-                        //             t.forEach(function (h) {
-                            //                 if (!~names.indexOf(h)) {
-                                //                     names.push(h)
-                                //                     names = [...names]
-
-                                //                 }
-                                //                 abv.data().toArray().forEach(function (s) {
-                                    //                     s = s.split(',');
-                                    //                     s.forEach(function (d) {
-                                        //                         //console.log(i)
-                                        //                         if (!~abvs.indexOf(d)) {
-                                            //                             abvs.push(d)
-                                            //                             //console.log(abvs)
-                                            //                             if (d === a) {
-                                                //                                 selects.append('<option value="' + a + '">' + b + '</option>');
-                                                //                                 if (b === h) {
-
-//                                     name.append('<option value="' + d + '">' + h + '</option>');
-//                                 }
-
-//                             }
-//                         }
-//                     });
-
-//                 })
-//             })
-
-//         })
-//     })
-// },
