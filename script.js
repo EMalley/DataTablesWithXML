98a3c14667b718273b94a87d3e7902cc37b7ac12
Eric Malley <EMalley998@gmail.com>
2021-04-27 09:44:31 -0400

    search by multiple ids working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var array = [];
 var fArray = [];
 var data;
+var n;
 function getData() {
     $.ajax({
         type: "GET",
@@ -19,27 +20,18 @@ function getData() {
 }
 
 function buildTable(response) {
-    // Setup - add a text input to each footer cell
-    $('#results thead tr').clone(true).appendTo('#results thead');
-    $('#results thead tr:eq(1) th').each(function (i) {
-        var title = $(this).text();
-        console.log(title)
-        if (title != 'date') {
-            
-            $(this).html('<input type="text" placeholder="Search ' + title + '" />');
-
-            $('input', this).on('keyup change', function () {
-                if (table.column(i).search() !== this.value) {
-                    table
-                        .column(i)
-                        .search(this.value)
-                        .draw();
-                }
-            });
-        }
-    }
-    );
     $(response).find("food").each(function () {
+
+        //console.log($("abv", this))
+        var arr = []
+        var ab = $("abv", this);
+        ab.each(function () {
+            arr.push($(this).text())
+        })
+
+
+        //console.log(arr)
+        // console.log(result)
 
         var url = $('url', this).text();
         var date = $(this).attr('date');
@@ -47,16 +39,17 @@ function buildTable(response) {
             date: date,
             name: $('name', this).text(),
             price: $('price', this).text(),
-            abv: $('abv', this).text(),
+            abv: arr,
             calories: $('calories', this).text(),
             description: $('description', this).text(),
             test: $('test', this).text(),
             url: url
         }
         array.push(item);
+        //console.log(array)
     });
 
-    var table = $("#results").DataTable({
+    $("#results").DataTable({
         data: array,
         autowidth: false,
         columns: [
@@ -69,20 +62,17 @@ function buildTable(response) {
             { "data": "price", className: "px200" },
             { "data": "abv", className: "px200" },
             { "data": "calories", className: "px200" },
-            {
-                "data": "description", className: "px200"
-            },
+            { "data": "description", className: "px200" },
             { "data": "test", className: "px200" },
             { "data": "url", className: "px200" },
 
         ],
         columnDefs: [
-
             {
                 targets: 1,
                 //autowidth:false,
                 render: function (d, t, r, m) {
-                  //  console.log(r)
+                    //  console.log(r)
 
                     r.name = "<a href=" + r.url + ">" + r.name + "</a>"
 
@@ -90,8 +80,6 @@ function buildTable(response) {
                 },
                 // render: function (d, t, r) { return d.substr(0, 30); },
                 // targets: 1,
-
-
             },
             // { targets: 1, render: function (d, t, r) { return d.substr(0, 30); } }
 
@@ -99,40 +87,41 @@ function buildTable(response) {
         "deferRender": true,
         initComplete: function () {
             var abv = this.api().column(3);
+            //console.log(abv)
             var columns = this.api($("#results")).data();
+
+
 
             var name = $('<select id="test" class="filter"><option class="subject" value="">All NAME</option></select>')
             name.appendTo('#triggerName')
                 .on('change', function () {
                     var val = $(this).val();
-                    //columns.search(val ? '^' + $(this).val() + '$' : val, true, false).draw();
-                    columns.search(val).draw()
+                    columns.search(val ? '' + val + '' : '', true, false).draw();
+                    //columns.search(val).draw()
                 });
 
             var selects = $('<select class="filter"><option value="">All ABVS</option></select>')
                 .appendTo('#triggerAbv')
-                .on('change', function () {
+            selects.on('change', function () {
+                name.empty().append("<option value=''>All Names</option>")
+                for (var i = 0; i < columns.length; i++) {
+                    var row = columns[i];
+                    //console.log(row.abv)
+
                     var val = $(this).val();
+                    console.log(val)
                     // val = val.search("^"+this.value + "$", true,true,false)
-                    abv.search(val ? '^' + $(this).val() + '$' : val, true, false).draw()
-                    name.empty().append("<option value=''>All Names</option>")
-                    for (var i = 0; i < columns.length; i++) {
-                        var row = columns[i];
-                        if (val == row.abv) {
-                            var seen = {};
-                            name.children().each(function () {
-                                var txt = $(this).text();
-                                console.log(txt)
-                                if (seen[txt])
-                                    $(this).remove();
-                                else
-                                    seen[txt] = true;
-                            });
+                    abv.search(val ? '' + $(this).val() + '' : val, true, false).draw()
+                    //console.log(row)
+                    for (var j = 0; j < row.abv.length; j++) {
+                        var a = row.abv[j];
+                        console.log(a)
+                        if (val === a) {
                             name.append('<option>' + row.description + '</option>');
                         }
-
                     }
-                });
+                }
+            });
 
             /////////////////////////
             var foodObj = {
@@ -144,24 +133,29 @@ function buildTable(response) {
                 habcabc: "HomeStle Test"
             }
             $.each(foodObj, function (a, b) {
-                var abvs = []
-
                 //get description based off of the ABV chosen.
-                abv.data().toArray().forEach(function (s) {
-                    s = s.split(',');
-                    s.forEach(function (d) {
-                        if (!~abvs.indexOf(d)) {
-                            abvs.push(d)
-                            //console.log(row)
-                            //console.log(abvs)
-                            if (d === a) {
-                                selects.append('<option value="' + d + '">' + b + '</option>')
-                            }
+                var arr = []
+                for (var j = 0; j < columns.length; j++) {
+                    var abv = columns[j].abv;
+                    for (var t = 0; t < abv.length; t++) {
+                        var x = abv[t]
+
+                        if (x === a) {
+                            //remove duplicates
+                            var seen = {};
+                            selects.children().each(function () {
+                                var txt = $(this).text();
+                                if (seen[txt])
+                                    $(this).remove();
+                                else
+                                    seen[txt] = true;
+                            });
+                            //append option to selection
+                            selects.append("<option value=" + x + ">" + b + "</option>")
+                            //console.log(selects)
                         }
-
-                    })
-                })
-
+                    }
+                }
             })
         },
 
@@ -169,7 +163,15 @@ function buildTable(response) {
 }
 
 
+// console.log(a)
+// for (var j = 0; j < columns.length; j++){
+//     var row = columns[j];
+//     console.log(row.abv);
 
+//     if(row.abv === a){
+//         selects.append('<option value="' +  + '">' + b + '</option>')
+//     }
+// }
 
 
 
